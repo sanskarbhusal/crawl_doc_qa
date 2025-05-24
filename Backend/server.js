a857f3ae0c5b65d337cc46f7fa893deeb72a9299
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-05-25 01:00:41 +0545

    la

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import cookiePaeser from "cookie-parser";
-import userRoutes from './src/routes/userRoute.js'
+import userRoutes from "./src/routes/userRoute.js";
 import connectDB from "./src/db/mongoDb.js";
 import googleRoute from "./src/controllers/googleController.js";
 import passport from "passport";
@@ -11,7 +11,10 @@ import session from "express-session";
 const app = express();
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173","http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:5000",
+  "http://crawldocqa.sanskarbhusal.com.np",
+];
 
 app.use(
   cors({
@@ -25,24 +28,23 @@ app.use(
   session({
     secret: "secretcode",
     resave: false,
-    saveUninitialized:false
+    saveUninitialized: false,
   })
 );
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookiePaeser());
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 // userRoute
-app.use("/api/v1",userRoutes);
-app.use("/",googleRoute)
+app.use("/api/v1", userRoutes);
+app.use("/", googleRoute);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
