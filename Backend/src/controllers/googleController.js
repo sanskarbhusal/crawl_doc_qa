48c23bdde96a319fb7e6c97499eec15ec3988b7f
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-05-25 00:46:54 +0545

    git is badass

--- Backend/src/controllers/googleController.js
+++ Backend/src/controllers/googleController.js
@@ -12,7 +12,8 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback",
+      callbackURL:
+        "http://crawldocqa.sanskarbhusal.com.np:5000/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, cb) => {
       try {
