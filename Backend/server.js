383d4db5fbe97cd86bcf2f80a2c57ad144c56152
sanskarbhusal <sanskarbhusal123@gmail.com>
2025-05-25 01:28:28 +0545

    fixing the fucking backend

--- Backend/server.js
+++ Backend/server.js
@@ -43,7 +43,7 @@ app.use(passport.session());
 app.use("/api/v1", userRoutes);
 app.use("/", googleRoute);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
